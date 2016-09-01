d06c0f332e13b8d774025b87da80e2c2dbb476d6
ForStar <forstar@vm1.local>
2016-09-01 03:30:48 +0200

    Initial Commit

---
+++ LGMcompanyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LGMcompanyApp
+//
+//  Created by ForStar on 9/1/16.
+//  Copyright © 2016 ForStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
